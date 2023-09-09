d1a815f42aef8fc93d100dd2612ecd4f285b6661
Jiri <wi22b004@technikum-wien.at>
2023-09-09 12:58:58 +0200

    Initial commit

---
+++ src/test/java/com/example/SLM_Test/SlmTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SLM_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
